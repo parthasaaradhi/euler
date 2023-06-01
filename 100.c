49792f769e3b591ff941cba8c1460a0a718e9244
venky <venkateshnagavolu@gmail.com>
2023-06-02 03:12:46 +0530

    100. b_square-4ac is perfect square.  Negative pell equation.

--- 100.c
+++ 100.c
@@ -1,26 +1,14 @@
 #include<stdio.h>
-
-int main()
-{
-	long long n = 1000000000000;
-	long long x,y,z,w,s,t;
-	int i = 0;
-	x = 29;
-	y = 41;
-	z = 169;
-	w = 239;
-	while(i<1)//w<2*n-1)
-	{
-		i++;
-		s = z;
-		t = w;
-		z = x*s+2*y*t;
-		w = x*t+y*s;
+#define limit 2000000000000
+int main() {
+	long long l,k,n,x,ln,kn;
+	l=1;k=1;
+	while(l<limit) {
+		kn=k;ln=l;
+		l = 3*ln+4*kn;
+		k = 2*ln+3*kn;
 	}
-	s = (z+1)/2;
-	t = (w+1)/2;
-	printf("%lld\n", (z+1)/2);
-	printf("%lld\n", (w+1)/2);
-	printf("%lld\n", (s*(s-1))/(t*(t-1)));
+	printf("%lld\n", (1+l)/2);
+	printf("%lld\n", (1+k)/2);
 	return 0;
 }
