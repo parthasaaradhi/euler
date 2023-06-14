95cfe2945810c6cb74ad5a50641efdb22d76f51d
venky <venkateshnagavolu@gmail.com>
2023-06-14 19:36:01 +0530

    103 and 105

--- 103.c
+++ 103.c
@@ -1,12 +1,59 @@
 #include<stdio.h>
-#include "hdr/105_spl_sub.h"
-
-int main()
-{
+#include<stdlib.h>
+int total_subsets;
+int set_size;
+int* subsums;
+void clean_slate() {
 	int i;
-	int a[7] = {20, 31, 38, 39, 40, 42, 45};
-	if (speacial_subset_test(7, a) != 0)
-		for(i=0;i<7;i++)
-			printf("%d", a[i]);
+	for(i=0;i<total_subsets;i++) subsums[i] = 0;
+}
+void compute_all_subsets(int* trial, int idx) {
+	int x=1;int i;
+	if(idx == set_size)
+		return;
+	x = x<<idx;
+	for(i=0;i<total_subsets;i++)
+		if((i&x) != 0) {
+			subsums[i] = subsums[i] + trial[idx];
+		}
+	compute_all_subsets(trial, idx+1);
+}
+int no_elemets(int x) {
+	int ans=0;
+	while(x!=0) {
+		ans = ans + x%2;
+		x=x/2;
+	}
+	return ans;
+}
+int test_the_trial(int* trial) {
+	int i,j,in,jn;
+	clean_slate();
+	compute_all_subsets(trial, 0);
+	for(i=1;i<total_subsets;i++) {
+		for(j=i+1;j<total_subsets;j++)
+			if((i&j)==0) {
+				in=no_elemets(i);
+				jn=no_elemets(j);
+				//printf("Comparing %d %d %d : %d %d %d\n", i, in, subsums[i], j, jn, subsums[j]);
+				if((in>jn && subsums[i]<=subsums[j]) || (in<jn && subsums[i]>=subsums[j]) || subsums[i]==subsums[j]) {
+					//printf("Not ok\n");
+					return 1;
+				}
+			}
+	}
+	return 0;
+}
+int main() {
+	int i;
+	int trial[7] = {20,31,38,39,40,42,45};
+	set_size = 7;
+	total_subsets = 1 << set_size;
+	subsums = malloc(total_subsets*sizeof(int));	
+	if(test_the_trial(trial)==0)
+		for(i=0;i<set_size;i++)
+		       printf("%d",trial[i]);
 	printf("\n");
+	free(subsums);
+	return 0;
 }
--- 105.c
+++ 105.c
@@ -1,20 +1,62 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-#include "hdr/105_input.h"
-#include "hdr/105_spl_sub.h"
-
-int main()
-{
-	int **v;
-	int i,ans;
-
-	v = input_numbers();
+int total_subsets;
+int set_size;
+int* subsums;
+int* trial;
+void clean_slate() {
+	int i;
+	for(i=0;i<total_subsets;i++) subsums[i] = 0;
+}
+void compute_all_subsets(int idx) {
+	int x=1;int i;
+	if(idx == set_size)
+		return;
+	x = x<<idx;
+	for(i=0;i<total_subsets;i++)
+		if((i&x) != 0) {
+			subsums[i] = subsums[i] + trial[idx];
+		}
+	compute_all_subsets(idx+1);
+}
+int no_elemets(int x) {
+	int ans=0;
+	while(x!=0) {
+		ans = ans + x%2;
+		x=x/2;
+	}
+	return ans;
+}
+int test_the_trial() {
+	int i,j,in,jn;
+	clean_slate();
+	compute_all_subsets(0);
+	for(i=1;i<total_subsets;i++) {
+		for(j=i+1;j<total_subsets;j++)
+			if((i&j)==0) {
+				in=no_elemets(i);
+				jn=no_elemets(j);
+				if((in>jn && subsums[i]<=subsums[j]) || (in<jn && subsums[i]>=subsums[j]) || subsums[i]==subsums[j])
+					return 1;
+			}
+	}
+	return 0;
+}
+int main() {
+	int i,j,ans,x;
 	ans = 0;
-	
-	for(i=0;i<100;i++)
-		ans = ans + speacial_subset_test(v[100][i], v[i]);
-	
+	for(j=1;j<=100;j++) {
+		scanf("%d", &set_size);
+		total_subsets = 1 << set_size;
+		trial = malloc(set_size*sizeof(int));
+		subsums = malloc(total_subsets*sizeof(int));
+		for(i=0;i<set_size;i++) scanf("%d", &trial[i]);
+		x = test_the_trial();
+		if(x==0)
+			for(i=0;i<set_size;i++) ans = ans + trial[i];
+		free(trial);
+		free(subsums);
+	}
 	printf("%d\n", ans);
 	return 0;
 }
