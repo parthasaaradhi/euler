015d02441473cc4bd7e3f5b94df7a871e7c2295c
venky <venkateshnagavolu@gmail.com>
2023-06-16 18:23:54 +0530

    101. Used Matrix Solver

--- 101.c
+++ 101.c
@@ -1,82 +1,70 @@
 #include<stdio.h>
-#include "stdlib.h"
-#include "hdr/101_linear_equations.h"
+#include<stdlib.h>
+#include "../implements/lib/matrix/matrix_long.h"
+long long* actual_result;
+long long** exponents;
+struct fraction ans;
 
-/*
-long long my_power(long long n, int i)
-{
-	long long x = 1;
-	while(i!=0)
-	{
-		x = n*x;
-		i--;
+void print_array(long long** e) {
+	long long i,j;
+	for(i=0;i<11;i++) {
+		for(j=0;j<11;j++)
+			printf("%lld\t", e[i][j]);
+		printf("\n");
 	}
-	return x;
 }
-*/
 
-long long evaluate_polynomial_expression(long long * a, int n, long long x)
-{
-	// a[0] + a[1] x + a[2] x^2 + a[3] x^3 + ... + a[n] x^n
-	int i;
-	long long p = 1;
-	long long ans = 0;
-	for(i=0;i<n+1;i++)
-	{
-		ans = ans + a[i]*p;
-		p = p*x;
+long long powers(long long n, long long x) {
+	long long ans;
+	ans = 1;
+	while(x--) {
+		ans = ans*n;
 	}
 	return ans;
 }
 
-int main()
-{
-	long long n;
-	long long* exp;
-	long long** system_of_equations;
-	long long* end_vector;
-	struct fraction** A;
-	struct fraction* b;
+long long given_polynomial(long long n) {
+	//return powers(n,3);
+	return ((powers(n,11) + 1)/(n+1));
+}
 
-	exp = malloc(11*sizeof(long long));
-	system_of_equations = malloc(5*sizeof(long long *));
-	end_vector = malloc(5*sizeof(long long));
-
-	int i;
-
-	for(n=1;n<6;n++)
-	{
-		system_of_equations[n-1] = malloc(5*sizeof(long long));
-		system_of_equations[n-1][0] = 1;
-		for(i=1;i<5;i++)
-			system_of_equations[n-1][i] = system_of_equations[n-1][i-1]*n;
+void try_n(long long n) {
+	struct matrix *A, *B;
+	struct fraction temp;
+	long long i;
+	A = matrix_create(n,n,exponents);
+	B = matrix_create_column(n,actual_result);
+	matrix_solve_linear_equations(A,B);
+	//fraction_set(&ans,0);
+	//matrix_print(B);
+	for(i=0;i<n;i++) {
+		fraction_set(&temp, exponents[n][i]);
+		fraction_mult(&temp, B->f[i], &temp);
+		fraction_add(&ans, &ans, &temp);
 	}
+	//printf("%lld %lld\n", ans.numerator, given_polynomial(n+1));
+	matrix_destroy(A);
+	matrix_destroy(B);
+}
 
-	exp[0] = 1;
+int main() {
+	long long i,j;
+	exponents = malloc(11*sizeof(long long*));
+	actual_result = malloc(11*sizeof(long long));
 
+	for(i=0;i<11;i++)
+		actual_result[i] = given_polynomial(i+1);
+	for(i=0;i<11;i++) {
+		exponents[i] = malloc(11*sizeof(long long));
+		exponents[i][0]=1;
+	}
+	for(i=0;i<11;i++)
+		for(j=1;j<11;j++)
+			exponents[i][j] = exponents[i][j-1]*(i+1);
+	//print_array(exponents);
+	fraction_set(&ans, 0);
 	for(i=1;i<11;i++)
-		exp[i] = -1*exp[i-1];
-
-	for(n=1;n<6;n++)
-	{
-		end_vector[n-1] = evaluate_polynomial_expression(exp, 10, n);
-		printf("%lld\n", end_vector[n-1]);
-	}
-
-	for(n=0;n<5;n++)
-	{
-		for(i=0;i<5;i++)
-			printf("%lld ", system_of_equations[n][i]);
-		printf("%lld\n", end_vector[n]);
-	}
-
-	A = convert_long_fraction_matrix(system_of_equations, 5);
-	b = convert_long_fraction_vector(end_vector, 5);
-
-	solve_linear_equations(A, b, 5);
-
-	print_matrix(A, 5);
-	print_vector(b, 5);
-
+		try_n(i);
+	printf("%lld\n", ans.numerator);
 	return 0;
 }
